e934d9cbd2ca5673a9f3cff0e543e1f4c25c6adc
nttzamos <nick.tzamos@gmail.com>
2021-11-15 17:29:38 +0200

    Implement completer searching methods

--- MainWidget/mainWidget.py
+++ MainWidget/mainWidget.py
@@ -10,25 +10,29 @@ class MainWidget(QWidget):
   searchingWidget = SearchingWidget()
   resultsWidget = ResultsWidget()
   
+  # Uncomment and change when complete
+  # dictionary_words = DBHandler.getAllWords()
+  dictionary_words = [
+    "Balcony", "Balloon", "Barcelona", "Balcony Light",
+    "Fan", "Room Light", "Brioche", "Basketball",
+    "Bedroom Heater", "Wall Switch"]
+  
   def __init__(self):
     super().__init__()
-    # self.setFixedWidth(1200)
     self.layout = QVBoxLayout(self)
     self.layout.setSpacing(0)
     self.layout.setContentsMargins(0, 0, 0, 0)
-
-    widget_names = DBHandler.getAllWords()
 
     # Search bar.
     self.searchbar = QLineEdit()
     self.searchbar.setContentsMargins(0, 0, 0, 0)
     # self.searchbar.textChanged.connect(self.update_display)
-    self.searchbar.returnPressed.connect(self.updateSearches)
+    self.searchbar.returnPressed.connect(self.searchWithEnter)
 
     # Adding Completer.
-    self.completer = QCompleter(widget_names)
+    self.completer = QCompleter(MainWidget.dictionary_words)
     self.completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
-    self.completer.activated.connect(self.updateSearches1)
+    self.completer.activated.connect(self.searchWithClick)
     self.searchbar.setCompleter(self.completer)
 
     self.searchbar.setPlaceholderText("Please enter a word.")
@@ -37,11 +41,16 @@ class MainWidget(QWidget):
     self.layout.addWidget(MainWidget.searchingWidget)
     self.layout.addWidget(MainWidget.resultsWidget)
 
-  def updateSearches(self):
-    self.addRecentSearch(self.searchbar.text())
-    self.searchbar.clear()
+  def searchWithEnter(self):
+    if self.searchbar.text() in MainWidget.dictionary_words:
+      self.addRecentSearch(self.searchbar.text())
+      self.searchbar.clear()
+    else:
+      # Implement showing necessary message
+      pass
 
-  def updateSearches1(self):
+  def searchWithClick(self, text):
+    self.addRecentSearch(text)
     QTimer.singleShot(0, self.searchbar.clear)
 
   def addRecentSearch(self, word):
